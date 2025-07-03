3200abde079e9f63dcfcfb9b5779e4f331427951
evgarnet77 <7107614ev@gmail.com>
2025-07-03 17:05:56 +0400

    Add project skeleton and license

---
+++ backend/gpt_handler.js
@@ -0,0 +1,5 @@
+// Placeholder for server-side recipe generation logic
+module.exports = async function generateRecipe(req, res) {
+  // TODO: integrate OpenAI API here
+  res.json({ message: 'Not implemented' });
+};
---
+++ public/script.js
@@ -0,0 +1,1 @@
+console.log('AI Kitchen app loaded');
